157d673e5cba1a7700d5a33fd2e84593c8ec1002
danielmunozamaya <danielmunozamaya2@gmail.com>
2025-03-17 20:55:03 +0100

    Conseguí, caralho!

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function pressButton() {
     document.querySelector(".contact-btn").classList.add("btn-pressed");
     document.querySelector(".contact-info").classList.add("contact-info-visible");
-    document.querySelector(".main-content-contact").classList.add("main-content-contact-change-display");
 }
