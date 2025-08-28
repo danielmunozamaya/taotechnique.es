64d883b7e35b2195123fecb689ded1ad3ed74a84
danielmunozamaya <danielmunozamaya2@gmail.com>
2025-08-28 05:55:24 +0200

    New web

--- script.js
+++
@@ -1,4 +0,0 @@
-function pressButton() {
-    document.querySelector(".contact-btn").classList.add("btn-pressed");
-    document.querySelector(".contact-info").classList.add("contact-info-visible");
-}
